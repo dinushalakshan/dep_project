82d01bb5347f35a738919d615406040683080adf
DinushaLakshan <dinusha9632@gmail.com>
2020-08-04 16:53:11 +0530

    git initialize

---
+++ src/AppInitializer.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.sql.SQLException;
+import db.DBConnection;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+      launch(args);
+      try {
+        DBConnection.getInstance().getConnection().close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+      Parent root = FXMLLoader.load(this.getClass().getResource("/view/MainForm.fxml"));
+      Scene mainScene = new Scene(root);
+      primaryStage.setScene(mainScene);
+      primaryStage.setTitle("Hello");
+      primaryStage.centerOnScreen();
+      primaryStage.show();
+    }
+
+  }
